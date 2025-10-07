80df4e2282e8ab16b80765e2cb49979ce1693d66
spanowix <tomek.filipowicz2010@gmail.com>
2025-10-07 14:34:02 +0200

    lekcja na temat wejscia

---
+++ wejscie.py
@@ -0,0 +1,17 @@
+#1. Wprowadzanie danych przez użytkownika do programu
+
+#1) utworzenie zmiennej
+#2) na ekranie pojawia się komunikat z inputa
+#3) program się zatrzymuje i czeka na podanie danych i zatwierdzenie tego ENTEREM
+#4) po wpisaniu i zatwierdzeniu ENTEREM to co wpiszemy trafia jako string do zmiennej
+imie = input('Podaj swoje imie')
+
+liczba = int(input('podaj liczbe'))
+print(type(liczba))
+
+liczba_z_przecinkiem = float(input('Podaj liczbe z przecinkiem'))
+print(type(liczba_z_przecinkiem))
+
+lista = list(input('podaj coś'))
+print(cos)
+print(type(cos))
