71ea24f77c160dd99e40954aa4ece66392d1af1d
tomek <tomek@gmail.com>
2025-10-26 22:39:32 +0100

    Praca domowa

--- instrukcje_warunkowe_zadania.py
+++ instrukcje_warunkowe_zadania.py
@@ -1,4 +1,5 @@
 #Rozwiązywanie równania kwadratowego
+import math
 
 # a = float(input('Podaj liczbe a =/= 0'))
 # b = float(input('Podaj liczbe b =/= 0'))
@@ -28,3 +29,49 @@ elif (pisemny_j_polski + pisemny_j_obcy + pisemny_dodatkowy + ustny_j_obcy + ust
     print('zdałeś z amnestią')
 else:
     print('nie zdałes!')
+
+
+
+wynik_z_testu = int(input('wynik z testu'))
+ocena = int(input('ocena'))
+
+if ocena >= 5 or wynik_z_testu >= 90:
+    print('Dostałeś się do grupy zaawansowanej!')
+else:
+    print('Dostałeś się do grupy podstawowej!')
+
+
+a = float(input('Podaj liczbe rzeczywistą a różną od 0'))
+b = float(input('Podaj liczbe rzeczywistą b'))
+c= float(input('Podaj liczbę rzeczywistą c'))
+
+if b == 0 and c == 0:
+    print('Równanie = 0')
+if b == 0 and c != 0:
+    if - c / a > 0:
+        x1 = (-c / a) **0.5
+        x2 = -(-c / a) **0.5
+        print(f'x1 == {x1} v x2 == {x2}')
+    if - c / a < 0:
+        print('nie ma rozwiązań')
+if c == 0 and b != 0:
+    x1 = 0
+    x2 = - (b/a)
+    print(f'x1 == {x1} v x2 == {x2}')
+if b != 0 and c != 0:
+    delta = b ** 2 - 4 * a * c
+    if delta > 0:
+        x1 = (-b + delta ** 0.5) / (2 * a)
+        x2 = (-b - delta ** 0.5) / (2 * a)
+        print(f'x1 == {x1} v x2 == {x2}')
+if delta == 0:
+    x0 = -b / (2 * a)
+    print('x1 == x2 = {}'.format(x0))
+if delta < 0:
+    print('nie ma rozwiązań')
+
+
+
+
+
+
