cf237a468561d964750a76877f98eec625fe59a7
spanowix <tomek.filipowicz2010@gmail.com>
2025-11-27 12:35:12 +0100

    e tam se sprawdzilem kartkowke

--- petle_powtorzenie.py
+++ petle_powtorzenie.py
@@ -12,5 +12,16 @@ for b in lista:
 # lista[2] - element znajdujący pod indeksem 2 = 89
 
 #2.2
-for k in range(4):
+'''for k in range(4):
     print(lista[k])
+
+slowo = 'Cześć'
+for x in range(100):
+    print(slowo)'''
+
+lista3 = [555555, 2, 355555, 3, 6, 5, 3, 2, 1]
+'''for x in range(0, 8, 2):
+    print(lista3[x])'''
+
+for d in range(len(lista3)):
+    print(lista3[d])
