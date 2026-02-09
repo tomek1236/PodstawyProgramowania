fdfbce32c53aff5e523791bee09e6eeb9dbfc759
spanowix <tomek.filipowicz2010@gmail.com>
2026-02-09 11:25:05 +0100

    dd

--- listy_zadanie.py
+++ listy_zadanie.py
@@ -72,4 +72,67 @@ print(lista_norm)'''
 lista = [123, 89, 5600, 432, 11, 45, 900, 12450, 1410, 390, 9999]
 '''lista = [x for x in lista if x > 1000 or x < 9999]'''
 print(lista)
-lista = [x for x in lista not (x >= 1000 and x <= 9999)]
+
+
+plansza = [
+    [3, 8, 1, 9],
+    [4, 6, 5, 2],
+    [7, 1, 8, 3],
+    [2, 9, 4, 6]
+]
+
+# Zadanie 6.1.
+'''
+#średnie w wierszach
+for x in plansza:
+    print(sum(x) / len(x))
+
+#średnie w kolumnach
+for i in range(len(plansza[0])):
+    suma = 0
+    for j in range(len(plansza)):
+        suma += plansza[j][i]
+    print(suma / len(plansza))'''
+
+# Zadanie 6.2.
+for i in range(len(plansza)):
+    for j in range(len(plansza[0])):
+        licznik = 0
+        srodkowy = plansza[i][j]  # szary element
+        '''if srodkowy < plansza[i - 1][j]:
+            licznik += 1
+        if srodkowy < plansza[i + 1][j + 1]:
+            licznik += 1'''
+
+        for x in range(i - 1, (i + 1) + 1):
+            for y in range(j - 1, (j + 1) + 1):
+                if x < 0:
+                    x = len(plansza) - 1
+                elif x > len(plansza) - 1:
+                    x = 0
+                if y < 0:
+                    y = len(plansza[0]) - 1
+                elif y > len(plansza[0]) - 1:
+                    y = 0
+                czerwony = plansza[x][y]
+                if srodkowy < czerwony:
+                    licznik += 1
+        print(i, j, licznik)
+#Zadanie 4
+tekst = 'LITWOOJCZYZNOMOJATYJESTESZJAKZDROWIEILECIETRZEBACENICTENTYLKOSIEDOWIEKTOCIESTRACILDZISPIEKNOSCTWACALEJOZDOBIEWIDZEIOPISUJEBOTESKNIEPOTOBIEPANNOSWIETACOJASNEJBRONISZCZESTOCHOWYIOSTREJSWIECISZBRAMIETYCOGRODZAMKOWYNOWOGRODZKIOCHRANIASZZJEGOWIERNMLUDEMJAKMNIEDZIECKODOZDROWIAPOWROCILASCUDYMGDYODPLACZACEJMATKIPODTWOJAOPIEKEOFIAROWANYMARTWAPODNIOSELMOJEPOWIEKEIZARAZMOGLEMPIESZODOTWYCZSWIATYNPROGUISTZAWROCONEZYCIEPODZIEKOWACBOGUTAKNASPOWROCISCUDYMNADOJCZYZNYLONOTYMCZASEMPRENOSMOJADUSZEUTESKNIONADOTYCHPAGORKOWLESNYCHDOTYCHLAKZIELONYCHSZEROKONADBLEKITNYMNIEMNEMROZCIAGNIONYCHDOTYCHPOLMALOWANYCHZBOZEMROZMAITEMWYZLACANYCHPSZENICAPOSREBRZANYCHZYTEMGDZIEBURSZTYNOWYSWIERZOPGRYKAJAKSNIEGBIALAGDZIEPANIENSKIMRUMIENCEMDZIECIELINAPALAAWSZYSTKOPRZEPASANEJAKBYWSTEGAMIEDZAZIELONANANIJZZADKACICHEGRUSZESIEDZA'
+
+# sposób 1
+suma = 0
+for x in tekst:
+    suma += ord(x)
+
+print(suma)
+
+# sposób 2
+print(sum(map(ord, tekst)))
+'''kody_ascii_map = map(ord, tekst)
+kody_ascii_lista = list(kody_ascii_map)
+suma2 = sum(kody_ascii_lista)
+print(suma2)'''
+
+
